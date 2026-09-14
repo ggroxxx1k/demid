a8e69d6fe9dea2edffcaac9f6c9a49da5a15d160
ggroxxx1k <misha2008dyakov@gmail.com>
2026-09-14 20:44:14 +0300

    Update adin.py

---
+++ first year/ASD/adin.py
@@ -0,0 +1,17 @@
+import math 
+a, b, c = [float(input(f'введите коэффициент {x}: ')) for x in ['a', 'b', 'c']]
+
+if a == 0:
+        raise ValueError("коэффициент 'a' не может быть нулем")
+
+D = b ** 2 - 4 * a * c
+
+if D > 0:
+    x1 = (-b + math.sqrt(D)) / (2 * a)
+    x2 = (-b - math.sqrt(D)) / (2 * a)
+    print("два корня:", x1, x2)
+elif D == 0:
+    x = -b / (2 * a)
+    print("один корень x =", x)
+else:
+    print("корней нет")
